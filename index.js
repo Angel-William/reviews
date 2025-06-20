dc6d7bb6c410fdee94ed2042e32ca68ab7c85351
AngelTheAnalyst <angelawilliam262@gmail.com>
2025-06-21 00:53:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ import random from "random";
 const path = "./data.json";
 
 const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+  // Start: Jan 1, 2024 in EAT, End: Jun 21, 2025 in EAT
+  const startDate = moment("2024-01-01T00:00:00+03:00");
+  const endDate = moment("2025-06-21T23:59:59+03:00");
+  
+  // Calculate total days in range
+  const totalDays = endDate.diff(startDate, 'days');
+  const randomDays = random.int(0, totalDays);
+  
+  const date = startDate.add(randomDays, 'days').format();
 
   const data = {
     date: date,
@@ -24,16 +27,23 @@ const markCommit = (x, y) => {
 
 const makeCommits = (n) => {
   if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  
+  // Start: Jan 1, 2024 in EAT, End: Jun 21, 2025 in EAT
+  const startDate = moment("2024-01-01T00:00:00+03:00");
+  const endDate = moment("2025-06-21T23:59:59+03:00");
+  
+  // Calculate total days in range
+  const totalDays = endDate.diff(startDate, 'days');
+  const randomDays = random.int(0, totalDays);
+  
+  const date = startDate.add(randomDays, 'days').format();
 
   const data = {
     date: date,
   };
   console.log(date);
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+    simpleGit().add([path]).commit(date, { "--date": date }, makeCommits.bind(this, --n));
   });
 };
 
